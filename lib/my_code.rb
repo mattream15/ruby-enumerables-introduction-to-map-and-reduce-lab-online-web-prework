f4e4413c384bc9ceb839fa586b66e06fdb62d4e2
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-12 20:01:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,58 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i = 0
+  while i < source_array.length do
+source_array[i] = source_array[i] * -1
+    i += 1
+  end
+  return source_array
+end
+
+def map_to_no_change(source_array)
+i = 0
+while i < source_array.length do
+  source_array[i] = source_array[i] * 1
+  i += 1
+end
+return source_array
+end
+
+def map_to_double(source_array)
+i = 0
+while i < source_array.length do
+  source_array[i] = source_array[i] * 2
+  i += 1
+end
+return source_array
+end
+
+def map_to_square(source_array)
+i = 0
+while i < source_array.length do
+  source_array[i] = source_array[i] ** 2
+  i += 1
+end
+return source_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+i = 0
+total = starting_point
+while i < source_array.length do
+total = total + source_array[i]
+i += 1
+end
+    return total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+  if source_array[i] = true
+    return true else
+  end
+  i += 1
+  if source_array[i] = false
+    return false
+end
+end
